88db67dd4b5efc3a6130d202038e49cd5ee07c9b
T00229172 <mun.li.pan@students.ittralee.ie>
2023-01-26 10:53:31 +0000

    Database stuff

---
+++ DBConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBA_Tickets_Retail
+{
+    class DBConnect
+    {
+        public const String oradb = "Data Source = oracle/orcl; User Id = T00229172; Password = fe4_Tdeegee6;";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,20 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
 
 namespace NBA_Tickets_Retail
 {
     internal static class Program
     {
+        private static OracleConnection conn;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            //When system shuts down
+            Application.ApplicationExit += new EventHandler(OnSysExit);
+
+            const String oradb = "Data Source = oracle/orcl; User Id = T00229172; Password = fe4_Tdeegee6;";
+            conn = new OracleConnection(oradb);
+            conn.Open();
+            MessageBox.Show("Database Connection status is " + conn.State.ToString(), "Connection State", MessageBoxButtons.OK, MessageBoxIcon.Information);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMainMenu());
+          
+        }
+
+        private static void OnSysExit(object sender, EventArgs e)
+        {
+            if (conn.State == ConnectionState.Open) conn.Close();
+        }
+
+        public static OracleConnection getOracleConnection()
+        {
+            return conn;
         }
     }
 }
