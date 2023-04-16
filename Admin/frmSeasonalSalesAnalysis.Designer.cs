ff885413deb3aeb29bce9239fece52ffe29686e7
Henry-0810 <Mun.Li.Pan@students.ittralee.ie>
2023-04-16 23:43:42 +0100

    done

--- Admin/frmSeasonalSalesAnalysis.Designer.cs
+++ Admin/frmSeasonalSalesAnalysis.Designer.cs
@@ -30,9 +30,9 @@ namespace NBA_Tickets_Retail
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSeasonalSalesAnalysis));
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.btnYRAback = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label6 = new System.Windows.Forms.Label();
@@ -40,9 +40,13 @@ namespace NBA_Tickets_Retail
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartSales = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.txtMostSales = new System.Windows.Forms.TextBox();
+            this.txtSumOfSales = new System.Windows.Forms.TextBox();
+            this.txtAvgSales = new System.Windows.Forms.TextBox();
+            this.txtLeastSales = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartSales)).BeginInit();
             this.SuspendLayout();
             // 
             // btnYRAback
@@ -54,6 +58,7 @@ namespace NBA_Tickets_Retail
             this.btnYRAback.Name = "btnYRAback";
             this.btnYRAback.Size = new System.Drawing.Size(57, 26);
             this.btnYRAback.TabIndex = 18;
+            this.btnYRAback.TabStop = false;
             this.btnYRAback.Text = "Back";
             this.btnYRAback.UseVisualStyleBackColor = false;
             this.btnYRAback.Click += new System.EventHandler(this.btnYRAback_Click);
@@ -61,7 +66,7 @@ namespace NBA_Tickets_Retail
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(947, 502);
+            this.pictureBox1.Location = new System.Drawing.Point(947, 528);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(118, 90);
@@ -87,7 +92,7 @@ namespace NBA_Tickets_Retail
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(12, 453);
+            this.label1.Location = new System.Drawing.Point(29, 459);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(105, 24);
             this.label1.TabIndex = 79;
@@ -99,7 +104,7 @@ namespace NBA_Tickets_Retail
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(12, 491);
+            this.label4.Location = new System.Drawing.Point(29, 497);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(105, 24);
             this.label4.TabIndex = 78;
@@ -111,7 +116,7 @@ namespace NBA_Tickets_Retail
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(12, 529);
+            this.label3.Location = new System.Drawing.Point(29, 535);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(129, 24);
             this.label3.TabIndex = 77;
@@ -123,27 +128,77 @@ namespace NBA_Tickets_Retail
             this.label9.BackColor = System.Drawing.Color.Transparent;
             this.label9.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.Color.Black;
-            this.label9.Location = new System.Drawing.Point(12, 567);
+            this.label9.Location = new System.Drawing.Point(29, 573);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(251, 24);
             this.label9.TabIndex = 76;
             this.label9.Text = "Total Sales for whole season:";
             // 
-            // chart1
+            // chartSales
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(16, 90);
-            this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Total Sales";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(1048, 350);
-            this.chart1.TabIndex = 80;
-            this.chart1.Text = "chart1";
+            this.chartSales.BorderlineColor = System.Drawing.Color.Black;
+            chartArea6.AxisX.Title = "Match ID";
+            chartArea6.AxisX.TitleFont = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea6.AxisY.Title = "Sales";
+            chartArea6.AxisY.TitleFont = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea6.Name = "ChartArea1";
+            this.chartSales.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chartSales.Legends.Add(legend6);
+            this.chartSales.Location = new System.Drawing.Point(16, 90);
+            this.chartSales.Name = "chartSales";
+            this.chartSales.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chartSales.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.DimGray};
+            series6.ChartArea = "ChartArea1";
+            series6.Font = new System.Drawing.Font("Calibri", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series6.Legend = "Legend1";
+            series6.Name = "Total Sales";
+            this.chartSales.Series.Add(series6);
+            this.chartSales.Size = new System.Drawing.Size(1035, 350);
+            this.chartSales.TabIndex = 80;
+            this.chartSales.TabStop = false;
+            this.chartSales.Text = "chart1";
+            // 
+            // txtMostSales
+            // 
+            this.txtMostSales.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMostSales.Location = new System.Drawing.Point(149, 458);
+            this.txtMostSales.Name = "txtMostSales";
+            this.txtMostSales.ReadOnly = true;
+            this.txtMostSales.Size = new System.Drawing.Size(197, 29);
+            this.txtMostSales.TabIndex = 81;
+            this.txtMostSales.TabStop = false;
+            // 
+            // txtSumOfSales
+            // 
+            this.txtSumOfSales.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSumOfSales.Location = new System.Drawing.Point(298, 572);
+            this.txtSumOfSales.Name = "txtSumOfSales";
+            this.txtSumOfSales.ReadOnly = true;
+            this.txtSumOfSales.Size = new System.Drawing.Size(154, 29);
+            this.txtSumOfSales.TabIndex = 82;
+            this.txtSumOfSales.TabStop = false;
+            // 
+            // txtAvgSales
+            // 
+            this.txtAvgSales.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAvgSales.Location = new System.Drawing.Point(164, 534);
+            this.txtAvgSales.Name = "txtAvgSales";
+            this.txtAvgSales.ReadOnly = true;
+            this.txtAvgSales.Size = new System.Drawing.Size(386, 29);
+            this.txtAvgSales.TabIndex = 83;
+            this.txtAvgSales.TabStop = false;
+            // 
+            // txtLeastSales
+            // 
+            this.txtLeastSales.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLeastSales.Location = new System.Drawing.Point(149, 496);
+            this.txtLeastSales.Name = "txtLeastSales";
+            this.txtLeastSales.ReadOnly = true;
+            this.txtLeastSales.Size = new System.Drawing.Size(197, 29);
+            this.txtLeastSales.TabIndex = 84;
+            this.txtLeastSales.TabStop = false;
             // 
             // frmSeasonalSalesAnalysis
             // 
@@ -151,8 +206,12 @@ namespace NBA_Tickets_Retail
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(1076, 604);
-            this.Controls.Add(this.chart1);
+            this.ClientSize = new System.Drawing.Size(1076, 630);
+            this.Controls.Add(this.txtLeastSales);
+            this.Controls.Add(this.txtAvgSales);
+            this.Controls.Add(this.txtSumOfSales);
+            this.Controls.Add(this.txtMostSales);
+            this.Controls.Add(this.chartSales);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -167,7 +226,7 @@ namespace NBA_Tickets_Retail
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmYearlyRevenueAnalysis_FormClosed);
             this.Load += new System.EventHandler(this.frmSeasonalSalesAnalysis_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartSales)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -182,6 +241,10 @@ namespace NBA_Tickets_Retail
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartSales;
+        private System.Windows.Forms.TextBox txtMostSales;
+        private System.Windows.Forms.TextBox txtSumOfSales;
+        private System.Windows.Forms.TextBox txtAvgSales;
+        private System.Windows.Forms.TextBox txtLeastSales;
     }
 }
--- Admin/frmSeasonalSalesAnalysis.cs
+++ Admin/frmSeasonalSalesAnalysis.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Data;
+using System.Drawing;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace NBA_Tickets_Retail
 {
@@ -25,7 +29,81 @@ namespace NBA_Tickets_Retail
 
         private void frmSeasonalSalesAnalysis_Load(object sender, EventArgs e)
         {
+            chartSales.Titles.Add("Sales chart");
+            chartSales.Titles[0].Font = new Font("Calibri", 16, FontStyle.Bold);
+            chartSales.ChartAreas[0].BackColor = Color.LightGray;
+            chartSales.ChartAreas[0].AxisX.Title = "Match ID";
+            chartSales.ChartAreas[0].AxisY.Title = "Total Sales (€)";
+            chartSales.ChartAreas[0].AxisX.TitleFont = new Font("Calibri", 12, FontStyle.Bold);
+            chartSales.ChartAreas[0].AxisY.TitleFont = new Font("Calibri", 12, FontStyle.Bold);
 
+            chartSales.DataSource = loadChart().Tables[0];
+            chartSales.DataBind();
+
+            int matchIdCount = loadChart().Tables[0].Rows.Count;
+            chartSales.ChartAreas[0].AxisX.Interval = Math.Max(1, matchIdCount / 10);
+
+            chartSales.Series["Total Sales"].XValueMember = "Match_ID";
+            chartSales.Series["Total Sales"].YValueMembers = "Total_Sales_Match";
+            chartSales.Series["Total Sales"].IsValueShownAsLabel = true;
+
+            chartSales.Series["Total Sales"].LabelFormat = "#,##0.#0";
+            chartSales.Series["Total Sales"].LabelForeColor = Color.Black;
+
+            chartSales.Series["Total Sales"]["PointWidth"] = "0.5";
+            chartSales.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chartSales.ChartAreas[0].AxisX.MinorGrid.Enabled = false;
+            chartSales.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+            chartSales.ChartAreas[0].AxisY.MinorGrid.Enabled = false;
+            getStats();
+            
+        }
+
+        private DataSet loadChart()
+        {
+            OracleConnection conn = Program.getOracleConnection();
+
+            string sqlQuery = "SELECT DISTINCT COALESCE(sa.Match_ID, m.Match_ID) AS Match_ID, " +
+                "COALESCE(SUM(sa.Total_Sales), 0) AS Total_Sales_Match FROM Matches m " +
+                "LEFT JOIN Sales sa ON sa.Match_ID = m.Match_ID GROUP BY COALESCE(sa.Match_ID, m.Match_ID) " +
+                "ORDER BY Match_ID";
+
+            DataSet ds = new DataSet();
+            OracleDataAdapter adapt = new OracleDataAdapter(sqlQuery, conn);
+            adapt.Fill(ds);
+            return ds;
+        }
+
+        private void getStats()
+        {
+            string mostSalesMatchID = "";
+            decimal mostSales = decimal.MinValue;
+            string leastSalesMatchID = "";
+            decimal leastSales = decimal.MaxValue;
+            float avgSales = 0;
+            decimal totSales = 0;
+
+            foreach (DataRow row in loadChart().Tables[0].Rows)
+            {
+                decimal sales = Convert.ToDecimal(row["Total_Sales_Match"]);
+                if(sales > mostSales)
+                {
+                    mostSales = sales;
+                    mostSalesMatchID = row["Match_ID"].ToString();
+                }
+                if(sales < leastSales)
+                {
+                    leastSales = sales;
+                    leastSalesMatchID = row["Match_ID"].ToString();
+                }
+                totSales += sales;
+            }
+            int numOfRows = loadChart().Tables[0].Rows.Count;
+            avgSales = (float)(totSales / numOfRows);
+            txtMostSales.Text = $"{mostSalesMatchID} - €{mostSales:N2}";
+            txtLeastSales.Text = $"{leastSalesMatchID} - €{leastSales:N2}";
+            txtAvgSales.Text = $"The average sales for {numOfRows} Matches is €{avgSales:N2}";
+            txtSumOfSales.Text = $"€{totSales:N2}";
         }
     }
 }
